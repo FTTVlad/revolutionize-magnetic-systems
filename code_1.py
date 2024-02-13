eb54d6c208bed01b0b7eb3b61394f1b5495161f2
Vladimir <156534228+FTTVlad@users.noreply.github.com>
2024-02-13 17:46:57 +0100

    Create code_1.py

---
+++ code_1.py
@@ -0,0 +1,16 @@
+    const data = generateRandomData();
+function main() {
+    return data;
+    for (let i = 0; i < 10; i++) {
+function generateRandomData() {
+
+main();
+        console.log("Random Number: " + item);
+    data.forEach(item => {
+
+}
+}
+    }
+    const data = [];
+        data.push(Math.floor(Math.random() * 100) + 1);
+    });
